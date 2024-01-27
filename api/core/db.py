9ff647560d30102e38a0bd361d5840d8cecaf529
akash <akashcsemu@gmail.com>
2024-01-28 00:54:17 +0600

    Test

--- api/core/db.py
+++ api/core/db.py
@@ -3,7 +3,7 @@ from typing import Annotated, AsyncIterator
 from fastapi import Depends
 from sqlalchemy.exc import SQLAlchemyError
 from .settings import config
-
+import ssl
 
 db_user_name = config.DB_USERNAME
 db_password = config.DB_PASSWORD
@@ -13,7 +13,11 @@ db_name = config.DB_NAME
 
 DatabaseURL = f'postgresql+asyncpg://{db_user_name}:{db_password}@{db_host}:{db_port}/{db_name}'
 
-async_engine = create_async_engine(url=DatabaseURL, echo=True)
+ssl_object = ssl.create_default_context()
+ssl_object.check_hostname = False
+ssl_object.verify_mode = ssl.CERT_NONE
+
+async_engine = create_async_engine(url=DatabaseURL, echo=True, connect_args={"ssl": ssl_object})
 async_session_local = async_sessionmaker(
     bind=async_engine,
     autoflush=False,
