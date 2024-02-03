3c86e747e269534f061050740bc18a090b057531
akash <akashcsemu@gmail.com>
2024-02-03 14:40:12 +0600

    Revert "Test"
    This reverts commit 9fe8c398bd0e594bbf684600e947baabe773b18e.

---
+++ tests/test_core.py
@@ -0,0 +1,19 @@
+from fastapi.testclient import TestClient
+from api.main import app
+
+client = TestClient(app)
+
+def test_root():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello World!"}
+
+def test_another():
+    response = client.get('/another')
+    assert response.status_code == 200
+    assert response.json() == { "messgae": "Hello another test"}
+
+def test_post_method():
+    response = client.post('/post-method')
+    assert response.status_code == 200
+    assert response.json() == { "message": "Hello post-method"}
