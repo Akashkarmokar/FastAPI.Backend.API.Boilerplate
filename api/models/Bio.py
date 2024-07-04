ae32b45c26a7fa7c321b791e964a747af5b3eacb
akash <akashcsemu@gmail.com>
2024-07-04 12:18:34 +0600

    CHECK

--- api/models/Bio.py
+++ api/models/Bio.py
@@ -9,7 +9,7 @@ class Bio(TimestampedModel):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[Text] = mapped_column(Text,nullable=True)
     note: Mapped[Text] = mapped_column(Text,nullable=True)
-    # designation: Mapped[Text] = mapped_column(Text, nullable=True) 
+    # designation: Mapped[Text] = mapped_column(Text, nullable=True)
     image_key: Mapped[Text] = mapped_column(Text, nullable=True) 
 
     
