1a62e6565effbe00054e6dc4ee035687f6f85bfd
akash <akashcsemu@gmail.com>
2024-07-07 19:21:22 +0600

    CHECK

--- alembic/versions/14ddfb1ece75_.py
+++ alembic/versions/14ddfb1ece75_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 9e68f32db013
+Revision ID: 14ddfb1ece75
 Revises: 
-Create Date: 2024-07-04 07:18:28.825254
+Create Date: 2024-07-07 13:19:05.200971
 
 """
 from typing import Sequence, Union
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '9e68f32db013'
+revision: str = '14ddfb1ece75'
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
@@ -24,6 +24,7 @@ def upgrade() -> None:
     sa.Column('id', sa.Integer(), nullable=False),
     sa.Column('name', sa.Text(), nullable=True),
     sa.Column('note', sa.Text(), nullable=True),
+    sa.Column('designation', sa.Text(), nullable=True),
     sa.Column('image_key', sa.Text(), nullable=True),
     sa.Column('created_at', sa.DateTime(), nullable=True),
     sa.Column('updated_at', sa.DateTime(), nullable=True),
